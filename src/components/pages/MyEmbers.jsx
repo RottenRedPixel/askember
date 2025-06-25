6e28942e7b6356c3e7964b0601268748987e3ce2
Amado Batour <amadobatour@hotmail.com>
2025-06-25 16:39:17 -0700

    Update rows icon to use 2-element design instead of 3

--- src/components/pages/MyEmbers.jsx
+++ src/components/pages/MyEmbers.jsx
@@ -22,7 +22,7 @@ const GridIcon = () => (
 
 const ScrollIcon = () => (
   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 10h16M4 14h16M4 18h16" />
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 6h18v4H3V6zM3 14h18v4H3v-4z" />
   </svg>
 );
 
